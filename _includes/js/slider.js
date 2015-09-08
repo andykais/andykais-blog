6fbb663538bba80f217e9f712ab979248861196a
AndrewKaiser <kaisea@rpi.edu>
2015-09-08 15:50:20 -0400

    working, left top align

--- _includes/js/slider.js
+++ _includes/js/slider.js
@@ -1,50 +1,103 @@
 $(document).ready(function () {
 	var srcs = [
-		'/img/landscape/15596853295_f58af35a10_o_72.jpg',
-		'/img/landscape/15867559253_79eddac6ef_o_72.jpg',
-		'/img/landscape/16299985978_a0703be4c9_o_72.jpg',
-		'/img/landscape/16301422639_2af15cd8ac_o_72.jpg',
-		'/img/landscape/20151857072_8f030baeb1_o_72.jpg',
-		'/img/landscape/15868736011_1bdac9d348_o_72.jpg'
-	];
+		'/img/landscape/DSC_0183.JPG',
+		'/img/landscape/DSC_0199.JPG',
+		'/img/landscape/DSC_1021_bright.jpg',
+		'/img/landscape/DSC_1570.JPG',
+		'/img/landscape/DSC_1576.JPG',
+		'/img/landscape/DSC_1731.JPG',
+		'/img/landscape/DSC_1841.JPG',
+		'/img/landscape/DSC_2341.JPG',
+		'/img/landscape/DSC_3613.JPG',
+		'/img/landscape/DSC_3616.JPG'
+	]
 	//snowshoes and backpack use uncropped version
+	loop(srcs)
+	
 
-	display_img(srcs[0])
-	var stream = Bacon.interval(10000);
-	// var stream = Bacon.interval(2000);
-	var i = 0;
-	stream.onValue(function (v) {
-		display_img(srcs[i]);
-
-		if (i >= srcs.length - 1) i=0;
-		else i++;
-		// console.log(srcs[i])
-	});
-	$(window).resize(function() {
-		display_img(srcs[i])
-	})
-	var circle = document.getElementById('face')
-	var img = document.createElement('img');
-	img.src = '/img/me/734163_710085245668602_1982396748_n_small.jpg';
-	circle.appendChild(img)
 });
-function orient() {
-	var imgHeight = document.getElementById('landscape').height;
-	var winHeight = window.innerHeight;
-	console.log(imgHeight)
-	console.log(winHeight)
-	if (imgHeight < winHeight) {
-		console.log('test')
-	}
-	else {
-		console.log('wrong')
-	}
+
+function loop(srcs) {
+	var value = srcs[0];
+	display_img(value)
+	var stream = Bacon.sequentially(100000, srcs);
+	stream.onValue(function (v) {
+		display_img(v)
+		value = v;
+	})
+	$(window).resize(function() {
+		display_img(value)
+	})
+	// var stream = Bacon.interval(2000);
+	// stream.subscribe(
+	// 	return function () {
+	// 	Bacon.repeat(function (i) {
+	// 		console.log(i);
+	// 	})
+	// })
+	// // var stream = Bacon.interval(2000);
+	// var i = 0;
+	// stream.onValue(function (v) {
+	// 	console.log('!slide!')
+	// 	display_img(srcs[i]);
+
+	// 	if (i >= srcs.length - 1) i=0;
+	// 	else i++;
+	// 	// console.log(srcs[i])
+	// });
 }
+
 function display_img(src) {
+
 	var header = document.getElementById('header-photo');
-	var img = header.childNodes[0];
-	img.src = src;
+	var img = document.getElementById('landscape')
 
 	img.width = window.innerWidth;
-	header.style.height = window.innerHeight + 'px';
+
+	// var imgHeight =img.height;
+	// var winHeight = window.innerHeight;
+	// //taller than wide
+	// if (imgHeight < winHeight) {
+	// 	header.style.height = imgHeight + 'px';
+	// }//wider than tall
+	// else {
+	// 	$(img).addClass(' wide-landscape');
+	// 	header.style.height = winHeight + 'px';
+	// }
+
+
+/*	$(img).off('load.landscape');
+
+	$(img).on('load.landscape', function(){
+		var imgHeight =img.height;
+		var winHeight = window.innerHeight;
+		//taller than wide
+		if (imgHeight < winHeight) {
+			header.style.height = imgHeight + 'px';
+		}//wider than tall
+		else {
+			$(img).addClass(' wide-landscape');
+			header.style.height = winHeight + 'px';
+		}
+
+	});
+*/	
+	img.onload = function () {
+		var imgHeight = img.height;
+		var winHeight = window.innerHeight;
+		//taller than wide
+		if (imgHeight < winHeight) {
+			header.style.height = imgHeight + 'px';
+			$(img).removeClass('wide-landscape')
+		}//wider than tall
+		else {
+			$(img).addClass('wide-landscape');
+			header.style.height = winHeight + 'px';
+		}
+	}
+	img.src = src;
+
+	header.style.background = 'url(' + src + ') no-repeat center center'
+	header.style['background-size'] = 'cover';
+	header.style['background-repeat'] = 'no-repeat';
 }
