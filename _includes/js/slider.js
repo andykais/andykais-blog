5305bb8f62b62e295602cd1729dc541d3da6d6ba
AndrewKaiser <kaisea@rpi.edu>
2015-09-08 19:21:32 -0400

    cover centers, cleaned up

--- _includes/js/slider.js
+++ _includes/js/slider.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 function loop(srcs) {
 	var value = srcs[0];
 	display_img(value)
-	var stream = Bacon.sequentially(100000, srcs);
+	var stream = Bacon.sequentially(10000, srcs);
 	stream.onValue(function (v) {
 		display_img(v)
 		value = v;
@@ -28,76 +28,32 @@ function loop(srcs) {
 	$(window).resize(function() {
 		display_img(value)
 	})
-	// var stream = Bacon.interval(2000);
-	// stream.subscribe(
-	// 	return function () {
-	// 	Bacon.repeat(function (i) {
-	// 		console.log(i);
-	// 	})
-	// })
-	// // var stream = Bacon.interval(2000);
-	// var i = 0;
-	// stream.onValue(function (v) {
-	// 	console.log('!slide!')
-	// 	display_img(srcs[i]);
-
-	// 	if (i >= srcs.length - 1) i=0;
-	// 	else i++;
-	// 	// console.log(srcs[i])
-	// });
 }
 
 function display_img(src) {
 
 	var header = document.getElementById('header-photo');
-	var img = document.getElementById('landscape')
+	var winHeight = window.innerHeight;
+	var winWidth = window.innerWidth;
+	// header.style.height = winHeight + 'px';
 
-	img.width = window.innerWidth;
-
-	// var imgHeight =img.height;
-	// var winHeight = window.innerHeight;
-	// //taller than wide
-	// if (imgHeight < winHeight) {
-	// 	header.style.height = imgHeight + 'px';
-	// }//wider than tall
-	// else {
-	// 	$(img).addClass(' wide-landscape');
-	// 	header.style.height = winHeight + 'px';
-	// }
-
-
-/*	$(img).off('load.landscape');
-
-	$(img).on('load.landscape', function(){
-		var imgHeight =img.height;
-		var winHeight = window.innerHeight;
-		//taller than wide
-		if (imgHeight < winHeight) {
-			header.style.height = imgHeight + 'px';
-		}//wider than tall
-		else {
-			$(img).addClass(' wide-landscape');
-			header.style.height = winHeight + 'px';
-		}
-
-	});
-*/	
-	img.onload = function () {
-		var imgHeight = img.height;
-		var winHeight = window.innerHeight;
-		//taller than wide
-		if (imgHeight < winHeight) {
-			header.style.height = imgHeight + 'px';
-			$(img).removeClass('wide-landscape')
-		}//wider than tall
-		else {
-			$(img).addClass('wide-landscape');
-			header.style.height = winHeight + 'px';
-		}
-	}
-	img.src = src;
 
 	header.style.background = 'url(' + src + ') no-repeat center center'
 	header.style['background-size'] = 'cover';
+	// header.style['background-size'] = '100%';
+	// header.style['background-size'] = 'contain';
 	header.style['background-repeat'] = 'no-repeat';
+
+	console.log('height ' + winHeight)
+	console.log('width ' + winWidth/1.5)
+
+	if (winHeight < winWidth/1.5) {
+		$(header).addClass('wide-landscape')
+		console.log('wide')
+		header.style.height = winHeight + 'px';
+	} else {
+		console.log('tall')
+		$(header).removeClass('wide-landscape')
+		header.style.height = winWidth/1.5 + 'px';
+	}
 }
