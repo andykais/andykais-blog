7aabdf03d85d976c291e547cf1bcebab66ae5f55
Andrew Kaiser <kaisea@rpi.edu>
2015-09-14 23:26:08 -0400

    cleaned up scar code

--- _includes/js/slider.js
+++ _includes/js/slider.js
@@ -18,7 +18,6 @@ $(document).ready(function () {
 });
 
 function loop(srcs) {
-	// var value = srcs[0];
 	// loop through array values
 	var stream = Bacon.repeatedly(15000, srcs);
 	// stream = Bacon.once(1).combine(stream)
@@ -31,11 +30,8 @@ function loop(srcs) {
 		loading[0] = loading[1];
 		loading[1] = v;
 		display_img(loading)
-		// value = v;
 	})
-	$(window).resize(function() {
-		// display_img(loading)
-	})
+	$(window).resize(function() {})
 }
 
 function display_img(loading) {
@@ -54,47 +50,6 @@ function display_img(loading) {
 	})
 	// console.log(insertBackground)
 	var img = bg.css("background-image", insertBackground);
-	// var img = bg.css("background-image").split(", ");
-	// console.log(img)
-	// console.log(src)
-	// img.push(newEntry);
-	// img = img.splice(1, 3)
-	// console.log(img);
-	// console.log(img[1])
-	// console.log(img[0])
-	// img[0] = img[1];
-	// img[1] = 'url(' + src + ')'
-	// img = img.concat(img[0]).splice(1, img.length).join(",");
-	// console.log(img)
-	// bg.css("background-image", img);
 
-/*	if (winHeight < winWidth/viewRatio) {
-		console.log('wide')
-		header.style.height = winHeight + 'px';
-		$(header).addClass('wide-landscape')
-		$(postContainer).addClass('side-post')
-		// $(postContainer).insertBefore(header)
-	} else {
-		console.log('tall')
-		$(header).removeClass('wide-landscape')
-		$(postContainer).removeClass('side-post')
-		header.style.height = winWidth/viewRatio + 'px';
-	}
-*/
-// header.style.height = winWidth + 'px';
 	header.style.height = winHeight*0.8 + 'px';
-
-	//gets the width of the image even though it is a css image
-	var imageSrc = header.style
-					.backgroundImage
-					.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-					.split(',')[0];
-	window.onload = function () {
-		var img = new Image();
-		img.src = imageSrc;
-
-		var imgWidth = img.width,
-			imgHeight = img.height;
-	}
-
 }
